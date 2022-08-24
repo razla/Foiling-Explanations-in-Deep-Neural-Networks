130b468d7d4157519c5c8ab2f530ef1b68ace0e3
snirvt <snirvt@gmail.comk>
2022-08-25 00:35:33 +0300

    fixed get_std_grad

--- src/stats.py
+++ src/stats.py
@@ -3,12 +3,18 @@
 import numpy as np
 import torch
 
-def get_new_std_1d(X, mean):
-    squared = torch.square(X - mean)
-    sum_squared = torch.sum(squared, axis=0)
-    mean_sum_squared = 1/X.shape[0] * sum_squared
-    scalar_std = torch.mean(mean_sum_squared)
-    return scalar_std.detach().cpu().item()
+
+def get_std_grad(normalized_rewards, noise_tensor, std, mean, is_scalar=True):
+    grad_std = 0 
+    for k in range(1,noise_tensor.shape[0]):
+        Xk = noise_tensor[k].detach().cpu().numpy().reshape(1,-1)
+        grad_K = normalized_rewards[k].item() * (-1) * ((std**2) - np.square(Xk) + 2*np.dot(mean, Xk.T).sum()  - np.square(mean))/(std**3)
+        if is_scalar:
+            grad_K = grad_K.mean()
+        grad_std += grad_K
+    grad_std /= (noise_tensor.shape[0]-1)
+    return grad_std
+
 
 def get_std_grad_scalar(normalized_rewards, noise_tensor, std, mean):
     grad_std = 0
@@ -18,6 +24,8 @@ def get_std_grad_scalar(normalized_rewards, noise_tensor, std, mean):
     grad_std /= (noise_tensor.shape[0]-1)
     return grad_std
 
+
+
 def get_mean_grad_scalar(normalized_rewards, noise_tensor, std, mean):
     mean_grad = 0
     for k in range(1,noise_tensor.shape[0]):
@@ -25,3 +33,6 @@ def get_mean_grad_scalar(normalized_rewards, noise_tensor, std, mean):
         mean_grad += np.mean(normalized_rewards[k].item() * (Xk - mean)/(std**2))
     mean_grad /= (noise_tensor.shape[0]-1)
     return mean_grad
+
+
+
