2148d1f5971873b28882c39eb474278f1b769e47
snirvt <snirvt@gmail.comk>
2022-09-05 18:28:00 +0300

    seed for load_images

--- src/utils.py
+++ src/utils.py
@@ -1051,7 +1051,8 @@ def get_mean_std(dataset):
         std = np.array([0.2471, 0.2435, 0.2616])
     return mean, std
 
-def load_images(n_imgs):
+def load_images(n_imgs, seed):
+    np.random.seed(seed=seed)
     dirs = np.random.choice(os.listdir(IMAGENET_PATH), n_imgs * 2)
     base_images = []
     target_images = []
