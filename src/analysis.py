df663731042cc04225f111446a689ee850a047d7
snirvt <snirvt@gmail.comk>
2022-09-20 17:40:47 +0300

    experiment data migrated to public, added more graphical results

--- src/analysis.py
+++ src/analysis.py
@@ -1,19 +1,23 @@
 
 from copyreg import pickle
+from stringprep import c22_specials
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
 
-argparser_names = listdir('argparser')
+# argparser_names = listdir('argparser')
+argparser_names = listdir('/cs_storage/public_datasets/results/argparser')
 file = argparser_names[-1]
 res = []
 headers = ['n_iter', 'n_pop', 'method', 'lr', 'max_delta', 'optimizer', 'weight_decay',
- 'compression_type', 'n_components', 'LS', 'SYN','MC_FGSM', 'uniPixel',
-  'momentum', 'pair_index', 'input_loss', 'output_loss', 'expl_loss']
+ 'compression_type', 'n_components', 'LS', 'SYN','MC_FGSM', 'uniPixel', 'momentum',
+#    'pair_index',
+    'input_loss', 'output_loss', 'expl_loss']
 for file in argparser_names:
-    argparser_dict = np.load(join('argparser', file), allow_pickle=True).item()
+    # argparser_dict = np.load(join('argparser', file), allow_pickle=True).item()
+    argparser_dict = np.load(join('/cs_storage/public_datasets/results/argparser', file), allow_pickle=True).item()
     locals().update(argparser_dict) # turn dict key,val to local variables
     experiment = f'n_iter_{n_iter}_n_pop_{n_pop}_method_{method}_lr_{lr}_max_delta_{max_delta}_opt_{optimizer}_w_decay_{weight_decay}'
     tmp = [n_iter, n_pop, method, lr, max_delta, optimizer, weight_decay]
@@ -63,19 +67,91 @@ for file in argparser_names:
     experiment += f'_prefactors_{str(prefactors)}'
     seed = 0
     experiment += f'_seed_{seed}'
+    tmp+=[[],[],[]]
     for i in range(100):
         try: # not all images ready
-            with open(f'loss_file/{experiment}_{i}.txt') as file:
+            # with open(f'loss_file/{experiment}_{i}.txt') as file:
+            with open(f'/cs_storage/public_datasets/results/loss_file/{experiment}_{i}.txt') as file:
                 lines = file.readlines()
                 lines = [line.rstrip() for line in lines]
                 input_loss = eval('['+lines[0].split('[')[1])[1:]
                 output_loss = eval('['+lines[1].split('[')[1])[1:]
                 expl_loss = eval('['+lines[2].split('[')[1])[1:]
-                res.append(tmp + [i, min(input_loss), min(output_loss), min(expl_loss)])
+                # res.append(tmp + [i, min(input_loss), min(output_loss), min(expl_loss)])
+                # res.append(tmp + [i, min(input_loss), min(output_loss), min(expl_loss)])
+                tmp[-3].append(min(input_loss))
+                tmp[-2].append(min(output_loss))
+                tmp[-1].append(min(expl_loss))
         except:
             pass
-
+    res.append(tmp)
 frame = pd.DataFrame(res,columns=headers)
 
+frame[frame['LS'] == True]['output_loss']
 
 
+frame.pivot(index = 'method', columns='output_loss')
+
+frame.pivot_table(index ='lr', columns ='method', values = 'input_loss')
+
+
+
+
+# 'input_loss', 'output_loss', 'expl_loss'
+import matplotlib.pyplot as plt
+for i, method in enumerate(frame['method'].unique()):
+    frame_method = frame[frame['method'] == method]
+    tmp = []
+    for loss_list in frame_method['expl_loss']:
+       tmp+=loss_list
+    plt.bar(i, height=np.mean(tmp), label=method)
+plt.legend()
+plt.savefig('test.png')
+plt.close()
+
+
+
+## 
+
+attack_order = choices=['lrp', 'guided_backprop', 'gradient',# 'integrated_grad',
+'pattern_attribution', 'grad_times_input']
+
+
+from collections import defaultdict
+frame_dict = defaultdict(list)
+frame_ = frame[['n_pop', 'method', 'lr', 'LS', 'input_loss', 'output_loss', 'expl_loss']]
+
+for index, f in frame_.iterrows():
+    title = f"n_pop={f['n_pop']}, lr={f['lr']}, LS={f['LS']}"
+    frame_dict[(f['n_pop'], f['lr'], f['LS'])].append([(title, f['method'], f['expl_loss'])])
+
+len(frame_dict.keys())
+
+fig, axs = plt.subplots(3, 3,figsize=(20, 20))
+for i, key in enumerate(frame_dict.keys()):
+    boxplot_data = []
+    boxplot_labels = []
+    boxplot_label_order = []
+    for _, f in enumerate(frame_dict[key]):
+        boxplot_data.append(np.array(f[0][2]))
+        boxplot_labels.append(f[0][1])
+        boxplot_label_order.append(attack_order.index(f[0][1]))
+    bp_dict = fig.axes[i].boxplot(boxplot_data, labels=boxplot_labels, positions=boxplot_label_order, meanline=True, showmeans=True)
+    fig.axes[i].set_title(f[0][0])
+    plt.setp(fig.axes[i].get_xticklabels(), rotation=15)
+
+    for k, line in enumerate(bp_dict['means']):
+        x, y = line.get_xydata()[1] # top of median line
+        fig.axes[i].text(x, y, "{:.2e}".format(np.mean(boxplot_data[k])), horizontalalignment='right')#, verticalalignment='bottom') # draw above, centered
+
+
+fig.suptitle('expl_loss',size=50)
+plt.savefig('test_2.png')
+plt.show()
+plt.close()
+
+
+# src = /home/snirvit/AttaXAI/src/argparser
+# dest = /cs_storage/public_datasets/results
+# cp -r /home/snirvit/AttaXAI/src/argparser /cs_storage/public_datasets/results
+# cp -r /home/snirvit/AttaXAI/src/loss_file /cs_storage/public_datasets/results
